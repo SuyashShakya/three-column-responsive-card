ad61481cacc79a8ce1cc7e68319230e5da62e3f0
Suyash Shakya <suyash.shakya@dragonlaw.com.hk>
2021-09-19 21:27:21 +0545

    fix: changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import {makeStyles} from '@material-ui/core/styles';
+import BoxContent from './component/BoxContent';
 import luxury from './images/icon-luxury.svg';
 import sedans from './images/icon-sedans.svg';
 import suvs from './images/icon-suvs.svg';
@@ -19,58 +18,6 @@ const useStyles = makeStyles((theme) => ({
       height: '100%',
       flexDirection: 'column'
     },
-  },
-  box: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
-    padding: 50,
-    paddingRight: 75,
-    maxWidth: 300,
-    minWidth: 300,
-    minHeight: 420,
-    backgroundColor: 'hsl(31, 77%, 52%)' ,
-    boxSizing: 'border-box',
-    [theme.breakpoints.down('sm')]: {
-      paddingRight: 50,
-      maxWidth: 255,
-      minWidth: 255,
-      minHeight: 380,
-    },
-    '&.firstBox': {
-      borderRadius: '10px 0 0 10px ',
-        [theme.breakpoints.down('sm')]: {
-          borderRadius: '10px 10px 0 0'
-        }
-    },
-    '&.secondBox': {
-      backgroundColor: 'hsl(184, 100%, 22%)',
-    },
-    '&.thirdBox': {
-      backgroundColor: 'hsl(179, 100%, 13%)',
-      borderRadius: '0 10px 10px 0 ',
-        [theme.breakpoints.down('sm')]: {
-          borderRadius: '0 0 10px 10px'
-        }
-    },
-  },
-  headingText: {
-    fontFamily: `Big Shoulders Display, cursive`,
-    color: 'hsl(0, 0%, 95%)'
-  },
-  descriptionText: {
-    fontFamily: `Lexend Deca, sans-serif`,
-    color: 'hsla(0, 0%, 100%, 0.75)',
-    fontWeight: 400
-  },
-  buttonStyle: {
-    borderRadius: 15,
-    fontFamily: `Lexend Deca, sans-serif`,
-  },
-  buttonText: {
-    fontFamily: `Lexend Deca, sans-serif`,
-    textTransform: 'none',
-    fontWeight: 600
   }
 }))
 
@@ -78,54 +25,27 @@ const App = () => {
   const classes = useStyles();
   return (
     <Box className={classes.root}>
-        <Box className={`${classes.box} firstBox`}>
-          <Box flex={1}>
-            <img src={sedans} alt='sedans' width={64} height={40} />
-            <br />
-            <Typography variant='h4' className={classes.headingText}>
-              SEDANS
-            </Typography> 
-            <br />
-            <Typography color='textSecondary' variant='body2' className={classes.descriptionText}>
-              Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.
-            </Typography>  
-          </Box>
-          <Button variant='contained' className={classes.buttonStyle}>
-            <Typography variant='subtitle2' className={classes.buttonText}>Learn More</Typography>
-          </Button>
-        </Box>
-        <Box className={`${classes.box} secondBox`}>
-          <Box flex={1}>
-            <img src={suvs} alt='suvs' width={64} height={40} />
-            <br />
-            <Typography variant='h4' className={classes.headingText}>
-              SUVS
-            </Typography> 
-            <br/>
-            <Typography color='textSecondary' variant='body2' className={classes.descriptionText}>
-              Take a SUV for its spacious interior, power, and versatality. Perfect for your next family vacation and off-road adventure.
-            </Typography>  
-          </Box>
-          <Button variant='contained' className={classes.buttonStyle}>
-            <Typography variant='subtitle2' className={classes.buttonText}>Learn More</Typography>
-          </Button>  
-        </Box>
-        <Box className={`${classes.box} thirdBox`}>
-          <Box flex={1}>
-            <img src={luxury} alt='luxury' width={64} height={40} />
-            <br />
-            <Typography variant='h4' className={classes.headingText}>
-              LUXURY
-            </Typography>
-            <br /> 
-            <Typography color='textSecondary' variant='body2' className={classes.descriptionText}>
-              Cruise in the best car brands without bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.
-            </Typography>  
-          </Box>
-          <Button variant='contained' className={classes.buttonStyle}>
-            <Typography variant='subtitle2' className={classes.buttonText}>Learn More</Typography>
-          </Button>   
-        </Box>
+      <BoxContent
+        boxNo='firstBox' 
+        image={sedans}
+        imageAlt='sedans'
+        title='SEDANS'
+        desc='Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'
+      />
+      <BoxContent 
+        boxNo='secondBox'
+        image={suvs}
+        imageAlt='suvs'
+        title='SUVS'
+        desc='Take a SUV for its spacious interior, power, and versatality. Perfect for your next family vacation and off-road adventure.'
+      /> 
+      <BoxContent
+        boxNo='thirdBox' 
+        image={luxury}
+        imageAlt='luxury'
+        title='LUXURY'
+        desc='Cruise in the best car brands without bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.'
+      /> 
     </Box>  
   )
 }
---
+++ src/component/BoxContent.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import {makeStyles} from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    box: {
+        display: 'flex',
+        flex: 1,
+        flexDirection: 'column',
+        padding: 30,
+        maxWidth: 250,
+        minWidth: 250,
+        minHeight: 420,
+        boxSizing: 'border-box',
+        [theme.breakpoints.down('sm')]: {
+            padding: 30,
+            maxWidth: 255,
+            minWidth: 255,
+            minHeight: 400,
+        },
+        '&.firstBox': {
+            backgroundColor: 'hsl(31, 77%, 52%)',
+            borderRadius: '10px 0 0 10px ',
+                [theme.breakpoints.down('sm')]: {
+                borderRadius: '10px 10px 0 0'
+                }
+        },
+        '&.secondBox': {
+            backgroundColor: 'hsl(184, 100%, 22%)',
+        },
+        '&.thirdBox': {
+            backgroundColor: 'hsl(179, 100%, 13%)',
+            borderRadius: '0 10px 10px 0 ',
+                [theme.breakpoints.down('sm')]: {
+                borderRadius: '0 0 10px 10px'
+                }
+        }
+    },     
+    headingText: {
+        fontFamily: `Big Shoulders Display, cursive`,
+        color: 'hsl(0, 0%, 95%)'
+    },
+    descriptionText: {
+        fontFamily: `Lexend Deca, sans-serif`,
+        color: 'hsla(0, 0%, 100%, 0.75)',
+        fontWeight: 400
+    },
+    buttonStyle: {
+        borderRadius: 20,
+        width: 150,
+        padding: 10,
+        fontFamily: `Lexend Deca, sans-serif`,
+        '&.firstBox': {
+            '&:hover': {
+                borderColor: '#ffff',
+                border: '1px solid',
+                backgroundColor: 'hsl(31, 77%, 52%)'
+            },
+        },
+        '&.secondBox': {
+            '&:hover': {
+                borderColor: '#ffff',
+                border: '1px solid',
+                backgroundColor: 'hsl(184, 100%, 22%)'
+            },
+        },
+        '&.thirdBox': {
+            '&:hover': {
+                borderColor: '#ffff',
+                border: '1px solid',
+                backgroundColor: 'hsl(179, 100%, 13%)'
+            },
+        },
+    },
+    buttonText: {
+        fontFamily: `Lexend Deca, sans-serif`,
+        textTransform: 'none',
+        fontWeight: 600,
+       
+        '&.firstBox': {
+            color: 'hsl(31, 77%, 52%)',
+            '&:hover': {
+                color: '#ffff'
+            },
+        },
+        '&.secondBox': {
+            color: 'hsl(184, 100%, 22%)',
+            '&:hover': {
+                color: '#ffff'
+            },
+        },
+        '&.thirdBox': {
+            color: 'hsl(179, 100%, 13%)',
+            '&:hover': {
+                color: '#ffff'
+            },
+        }
+    }
+}))
+
+const BoxContent = ({image, imageAlt, title, desc, boxNo}) => {
+    const classes = useStyles()
+    return (
+        <Box className={`${classes.box} ${boxNo}`}>
+            <Box flex={1}>
+                <img src={image} alt={imageAlt} width={64} height={40} />
+                <br /> <br />
+                <Typography variant='h4' className={classes.headingText}>
+                    {title}
+                </Typography> 
+                <br/>
+                <Typography color='textSecondary' variant='subtitle2' className={classes.descriptionText}>
+                    {desc}
+                </Typography>  
+            </Box>
+            <Button variant='contained' className={`${classes.buttonStyle} ${boxNo}`}>
+                <Typography variant='subtitle2' className={`${classes.buttonText} ${boxNo}`}>Learn More</Typography>
+            </Button>
+        </Box>    
+    )
+}
+
+export default BoxContent;
